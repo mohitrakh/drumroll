2de689fd7c54375dc1dce491afcca730b9444e9c
mohitrakh <89239016+mohitrakh@users.noreply.github.com>
2022-10-09 19:56:21 +0530

    Added files
    files are added

---
+++ index.js
@@ -0,0 +1,60 @@
+let dumnum = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < dumnum; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    let name = this.innerHTML;
+   makesound(name);
+   btnanimate(name);
+  });
+};
+
+document.addEventListener('keypress', function(event){
+
+ makesound(event.key);
+ btnanimate(event.key);
+});
+
+function makesound(soundev){
+    switch (soundev) {
+        case "w":
+          var tom1 = new Audio("sounds/tom-1.mp3");
+          tom1.play();
+          break;
+        case "a":
+          var tom2 = new Audio("sounds/tom-2.mp3");
+          tom2.play();
+          break;
+        case "s":
+          var tom3 = new Audio("sounds/tom-3.mp3");
+          tom3.play();
+          break;
+        case "d":
+          var tom4 = new Audio("sounds/tom-4.mp3");
+          tom4.play();
+          break;
+        case "j":
+          var snare = new Audio("sounds/snare.mp3");
+          snare.play();
+          break;
+        case "k":
+          var crash = new Audio("sounds/crash.mp3");
+          crash.play();
+          break;
+        case "l":
+          var kick = new Audio("sounds/kick-bass.mp3");
+          kick.play();
+          break;
+        default: alert('wrong button onii chaan');
+          break;
+      }
+}
+
+function btnanimate(keypressed){
+    var keyanimate = document.querySelector("." + keypressed);
+    keyanimate.classList.add('pressed');
+
+    setTimeout(() => {
+        keyanimate.classList.remove('pressed');
+    }, 100);
+
+}
